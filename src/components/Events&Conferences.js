9f74eb8b72353d97ca656755a2e8bd6f5219612c
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2020-11-27 00:37:32 +0000

    solved issue in LocateUs.js and Event&Conferences.js

--- src/components/Events&Conferences.js
+++ src/components/Events&Conferences.js
@@ -15,15 +15,23 @@ const EventsAndConferences = () => {
             }
         }
     `)
-const Fluid = data.imageSharp.fluid;
+
     return (
-        <div className="Container_ mb-5 mt-5">
-            <h2 className="head-title_">Events and conferences</h2>
-            <div className="img">
-                <Img fluid={Fluid && data.imageSharp.fluid} />
-            </div>
-            <Button variant="contained" className="btn mt-3 " color="primary" size="large" href="/events-and-conferences/" >View</Button>
+      <div className="Container_ mb-5 mt-5">
+        <h2 className="head-title_">Events and conferences</h2>
+        <div className="img">
+            {data.imageSharp && (<Img fluid={data.imageSharp.fluid} />)}
         </div>
+        <Button
+          variant="contained"
+          className="btn mt-3 "
+          color="primary"
+          size="large"
+          href="/events-and-conferences/"
+        >
+          View
+        </Button>
+      </div>
     );
 }
 
